7a6ec28365c77b5ad3790cbf2662721757d835f0
Artem Kvasnetskyi <artem.kvasnetskyi@chisw.com>
2022-01-24 17:50:35 +0200

    - Add description to swinject code.

--- MVVMSkeleton/Application/AppContainer.swift
+++ MVVMSkeleton/Application/AppContainer.swift
@@ -17,8 +17,6 @@ protocol AppContainer: AnyObject {
     var characterService: CharactersService { get }
 }
 
-// TODO: - Add comments about Assembly
-// TODO: - When u will add Swinject code to proj – add descriptions to all of the methods
 final class AppContainerImpl: AppContainer {
     // MARK: - Public Properties
     public var appConfiguration: AppConfiguration { resolve() }
@@ -28,6 +26,12 @@ final class AppContainerImpl: AppContainer {
     public var characterService: CharactersService { resolve() }
     
     // MARK: - Private Properties
+    /// Swinject class represents a dependency injection container, which stores registrations of services and retrieves registered services with dependencies injected.
+    ///
+    /// Used as a private property, and not used outside the class. This is done so that if you change the library you don't have to change the application code.
+    ///
+    /// More information about the container and working with it can be found here:
+    /// https://github.com/Swinject/Swinject/blob/master/Documentation/DIContainer.md
     private let container = Container()
     
     // MARK: - Init
@@ -38,6 +42,15 @@ final class AppContainerImpl: AppContainer {
 
 // MARK: - Private Methods
 private extension AppContainerImpl {
+    /// In this example, all services are registered directly into the container.
+    ///
+    /// Learn how to register services here:
+    /// https://github.com/Swinject/Swinject/blob/master/Documentation/DIContainer.md#registration-in-a-di-container
+    ///
+    /// If your project has many services and you want to split their registration into modules, you can use **Assembler** and **Assembly**.
+    ///
+    /// More about Assembler and Assembly:
+    /// https://github.com/Swinject/Swinject/blob/master/Documentation/Assembler.md
     func registerServices() {
         registerAppConfiguration()
         registerUserService()
@@ -48,6 +61,10 @@ private extension AppContainerImpl {
         registerCharactersService()
     }
     
+    /// A lightweight method that return a specific service from the Swinject container.
+    ///
+    /// You can see more examples of how to use the resolve method here:
+    /// https://github.com/Swinject/Swinject/blob/master/Documentation/DIContainer.md
     func resolve<T>() -> T {
         container.resolve(T.self)!
     }
@@ -56,6 +73,10 @@ private extension AppContainerImpl {
         container.register(AppConfiguration.self) { _ in
             AppConfigurationImpl()
         }
+        /// The first time you invoke the resolve method with Container Object Scope, Swinject caches the service, and returns the same instance each subsequent time.
+        ///
+        ///  You can read more about this and other types of object scope here:
+        ///  https://github.com/Swinject/Swinject/blob/master/Documentation/ObjectScopes.md
         .inObjectScope(.container)
     }
     
@@ -74,13 +95,19 @@ private extension AppContainerImpl {
             
             return TokenPlugin(userService)
         }
-        .inObjectScope(.transient)
     }
     
     func registerNetworkingHandler() {
         container.register(NetworkingHandler.self) { _ in
             NetworkingHandler()
         }
+        /// The initCompleted method is used when we need to make a property injection after the other services in the graph have been initialised.
+        ///
+        /// **This method helps to solve Circular Dependencies.** In this case the method is used because NetworkingHandler requires TokenService, and at the same time TokenService requires NetworkingHandler.
+        /// With initCompleted and property injection we initialise the NetworkingHandler with empty properties, and only after the whole graph is initialised we will add TokenService to it.
+        ///
+        /// More about Circular Dependencies here:
+        /// https://github.com/Swinject/Swinject/blob/master/Documentation/CircularDependencies.md#circular-dependencies
         .initCompleted { resolver, handler in
             handler.tokenService = resolver.resolve(TokenService.self)!
             handler.userService = resolver.resolve(UserService.self)!
@@ -131,9 +158,8 @@ private extension AppContainerImpl {
         container.register(CharactersService.self) { resolver in
             let appConfiguration = resolver.resolve(AppConfiguration.self)!
             let networkingHandler = resolver.resolve(NetworkingHandler.self)!
-            let userService = resolver.resolve(UserService.self)!
+            let tokenPlugin = resolver.resolve(TokenPlugin.self)!
             let jsonPugin = JSONContentPlugin()
-            let tokenPlugin = TokenPlugin(userService)
             
             let provider = CNProvider(
                 baseURL: appConfiguration.environment.storageBaseURL,
